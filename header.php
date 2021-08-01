d1d61b9bd67705df0f4a731e3e72a789200b9c73
MESHach12345 <meshachadriansamuel@gmail.com>
2021-08-01 15:32:01 +0700

    Initial Commits

---
+++ header.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
+<div class="site" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div id="wrapper-navbar">
+
+		<div class="top-header">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-5 col-md-6">
+						<div class="mobile-holder-logo">
+							<a href="/">
+								<img src="<?php echo get_template_directory_uri(); ?>/image/logo.png" alt="Logo"> <!-- The get_template_directory_uri ensures that the selector is in the west-coast directory -->
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-7 col-md-6">
+						<div class="top-header-contact">
+							<a href="tel: 1800927831"><i class="fa fa-phone"></i>1800 927 831</a>
+							<a href="mailto: info@westcoastwaste.com.au" class="top-header-contact-email"><i class="fa fa-envelope-o"></i>info@westcoastwaste.com.au</a>
+							<div class="mobile-menu-dropdown">
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+									<span>Menu</span><i class="fa fa-bars"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<nav id="main-nav" class="navbar navbar-expand-xl navbar-dark" aria-labelledby="main-nav-label">
+
+			<div class="container">
+
+				<div class="search-input-holder">
+					<div class="title">
+						Quote:
+					</div>
+					<input type="text" placeholder="Type your Suburb Here">
+				</div>
+
+				<div class="mobile-menu-dropdown">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+						<span>Menu</span><i class="fa fa-bars"></i>
+					</button>
+				</div>
+
+				<!-- The WordPress Menu goes here -->
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav ml-auto',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'depth'           => 2,
+						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+					)
+				);
+				?>
+			</div><!-- .container -->
+
+		</nav><!-- .site-navigation -->
+
+	</div><!-- #wrapper-navbar end -->
+</div>
---
+++ page-templates/template-home.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Template Home
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+
+if ( is_front_page() ) {
+	get_template_part( 'global-templates/hero' );
+}
+?>
+
+<div class="home-hero-banner-top">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="skip-bin-info-holder">
+					<div class="title">
+						<i class="fa fa-arrow-circle-up"></i> Skip Bin Instant Quote
+					</div>
+					<div class="desc">
+						Simply Type your Suburb Above and you will be provided with an instant price.
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<img src="<?php echo get_template_directory_uri() ?>/image/home-hero-right-info.svg" alt="West Coast Info" width="100%">
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+get_footer();
